543f7cd9aa1e26a0f87a5e4783ca7b0fcfb2bb63
Raden Farhan Fauzan Rahayu <157620711+Rprhnr@users.noreply.github.com>
2025-01-12 20:03:02 +0700

    ubah database

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ global.app = express();
 global.knex = require("knex")({
   client: "mysql",
   connection: {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
+    host: "mysql.bacod.id",
+    port: 443,
+    user: "kelompok3",
+    password: "33445566",
     database: "travelapp",
   },
 });
