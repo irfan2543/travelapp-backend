d52c57dcb1bd30f9208dc6875dce14dc34f15709
irfan2543 <muhammadirfansyah66@gmail.com>
2025-01-16 18:40:19 +0700

    Add User Check and seat

--- module/login.js
+++ module/login.js
@@ -3,26 +3,26 @@ const { app } = global;
 
 app.post('/login', async (req, res) => {
     const { email, hashed_password } = req.body;
-
+    
     if (!email || !hashed_password) {
-        return res.status(400).json({ message: 'Email dan password harus diisi' });
+        return res.status(400).json({ message: 'Email dan password harus diisi'}); 
     }
 
     try {
         const user = await knex('users').where({ email }).first();
 
         if (!user) {
-            return res.status(401).json({ message: 'Email' });
+            return res.status(401).json({ message: 'Email Salah' });
         }
 
         const hashedPassword = crypto.createHash('md5').update(hashed_password).digest('hex');
 
         if (user.hashed_password !== hashedPassword) {
-            return res.status(401).json({ message: 'password salah' });
+            return res.status(401).json({ message: 'Password Salah' });
         }
 
         // Jika berhasil login, kembalikan userId dan first_name (opsional, diatur pada register.js)
-        res.status(200).json({ message: 'Login berhasil', userId: user.user_id, firstName: user.first_name, lastname: user.last_name, email:user.email });
+        res.status(200).json({ message: 'Login berhasil', userId: user.user_id, firstName: user.first_name, lastname: user.last_name, email:user.email, roles:user.roles });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Terjadi kesalahan saat login' });
